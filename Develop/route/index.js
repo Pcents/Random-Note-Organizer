7d7f4930dd8ddf3d35171da75bca46ff5b2b600d
James Plasencia <jamesplasencia@gmail.com>
2022-07-14 15:14:29 -0700

    touched up the routes and errors in the index. still errors

--- Develop/route/index.js
+++ Develop/route/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // Import our modular router for /notes
-const notesRouter = require('./notes');
+const notesRouter = require('./note');
 // import out db
 const dbRouter =require('../db/db.json');
 
 const app = express();
 
-app.use('/notes', notesRouter);
+app.use('./note', notesRouter);
 // TODO: Initialize diagnostics route
 app.use('../db/db.json', dbRouter);
 
