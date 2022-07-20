a22604b9f31bac8597921771e1a85f0221261aee
James Plasencia <jamesplasencia@gmail.com>
2022-07-19 18:07:50 -0700

    fixed heroku push

--- route/note.js
+++ route/note.js
@@ -26,8 +26,6 @@ noteRou.post('/', (req, res) => {
     }
   });
 
-  // * `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-  
 noteRou.delete('/api/notes/:id',(req,res)=>{
   const noteId = req.params.id
   readFromFile('./db/db.json')
--- server.js
+++ server.js
@@ -2,21 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require('./route/index');
-// create variables for the routes
-
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 app.use(express.static('public'));
-
-
-// `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-
-
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
